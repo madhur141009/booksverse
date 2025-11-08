2ddb0f8d13135a50dc221ccf7e4e8d70f4154b7a
Madhur Garg <137780454+madhur141009@users.noreply.github.com>
2025-11-08 17:49:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,11 @@
-/*=============== SEARCH ===============*/
-const searchButton = document.getElementById('search-button'),
-  searchClose = document.getElementById('search-close'),
-  searchContent = document.getElementById('search-content')
-
-// ===== SEARCH SHOW =====
-if (searchButton) {
-  searchButton.addEventListener('click', () => {
-    searchContent.classList.add('show-search')
-  })
-}
-
-// ===== SEARCH HIDDEN =====
-if (searchClose) {
-  searchClose.addEventListener('click', () => {
-    searchContent.classList.remove('show-search')
-  })
-}
-
-/*=============== HOME SWIPER ===============*/
 let swiperHome = new Swiper('.home__swiper', {
   loop: true,
-  spaceBetween: 5,   // changed from -24
+  spaceBetween: 5,
   grabCursor: true,
   slidesPerView: 'auto',
-  centeredSlides: true, // fix centering
+  centeredSlides: true,
   autoplay: {
-    delay: 1000, // lowercase "delay"
+    delay: 1000,
     disableOnInteraction: false,
   },
 });
